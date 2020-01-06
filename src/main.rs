523f7c5045913c832c55a673260ece64326aabf4
Odin Dutton <odindutton@gmail.com>
2020-01-06 11:10:59 +1100

    Support GZipped files

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
+use flate2::read::GzDecoder;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
 use vgm::parser;
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     let filename = args.get(1).expect("no file provided");
-    let f = File::open(&filename).unwrap();
-    let mut reader = BufReader::new(f);
-    let mut header = [0; 256];
-    reader.read_exact(&mut header).unwrap();
+    let mut f = File::open(&filename).unwrap();
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).unwrap();
 
-    let header = parser::header(&header).unwrap().1;
+    let mut gz = GzDecoder::new(&buffer[..]);
+    let mut buffer = if let Some(_) = gz.header() {
+        let mut buffer = Vec::new();
+        gz.read_to_end(&mut buffer).unwrap();
+        buffer
+    } else {
+        buffer
+    };
+
+    let header = parser::header(&mut buffer).unwrap().1;
 
     dbg!(header);
 }
